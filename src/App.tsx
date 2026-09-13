151124d3b5288bfd4bc41322e6f67e2f20c04476
SUMAYAAKTERDANA <info.sumaya935@gmail.com>
2026-09-13 14:45:27 +0600

    Update index js

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,9 @@ import Footer from "./components/Footer";
 import type { ItechList } from "./types/Techlisttypes";
 import "react-toastify/dist/ReactToastify.css";
 
-// Promise
-// const TechListPromise: Promise<ItechList[]> = fetch("/data.json").then(
-//   (res) => res.json()
-// );
-const TechListPromise: Promise<ItechList[]> = new Promise(
-  (resolve, reject) => {
-    setTimeout(() => {
-      fetch("/data.json")
-        .then((res) => res.json())
-        .then(resolve)
-        .catch(reject);
-    }, 2000);
-  }
+Promise
+const TechListPromise: Promise<ItechList[]> = fetch("/data.json").then(
+  (res) => res.json()
 );
 
 
